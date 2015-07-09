a17c1963a365033620ab6148a470ae749c3dcbd0
Thilo Habenreich <t.habenreich@web.de>
2015-07-09 18:25:06 +0200

    Add some more comments.

--- src/Xtractor/Http/Request.php
+++ src/Xtractor/Http/Request.php
@@ -4,14 +4,39 @@ if (!class_exists('Xtractor_Client')) {
   require_once dirname(__FILE__) . '/../autoload.php';
 }
 
+/**
+ * Class Xtractor_Http_Request
+ *
+ * This class delivers a business object to handle requests in a defined
+ * structure.
+ */
 class Xtractor_Http_Request
 {
+  /**
+   * @var string
+   */
   private $requestUrl;
+  /**
+   * @var string
+   */
   private $requestMethod = 'GET';
+  /**
+   * @var Xtractor_Http_Header
+   */
   private $requestHeader;
+  /**
+   * @var Xtractor_Http_Body
+   */
   private $requestBody;
+  /**
+   * @var Xtractor_Http_Options
+   */
   private $requestOptions;
 
+  /**
+   * @param $method
+   * @throws Xtractor_Http_Exception
+   */
   public function setRequestMethod($method)
   {
     $method = strtoupper($method);
@@ -24,26 +49,42 @@ class Xtractor_Http_Request
     $this->requestMethod = strtoupper($method);
   }
 
+  /**
+   * @return string
+   */
   public function getRequestMethod()
   {
     return $this->requestMethod;
   }
 
+  /**
+   * @param $requestUrl
+   */
   public function setUrl($requestUrl)
   {
     $this->requestUrl = $requestUrl;
   }
 
+  /**
+   * @return mixed
+   */
   public function getUrl()
   {
     return $this->requestUrl;
   }
 
+  /**
+   * @param Xtractor_Http_Header $headers
+   */
   public function setRequestHeader(Xtractor_Http_Header $headers)
   {
     $this->requestHeader = $headers;
   }
 
+  /**
+   * @return mixed
+   * @throws Xtractor_IO_Exception
+   */
   public function getRequestHeader()
   {
     if ( !method_exists($this->requestHeader, 'getFieldStrings') ) {
@@ -53,21 +94,33 @@ class Xtractor_Http_Request
     return $this->requestHeader->getFieldStrings();
   }
 
+  /**
+   * @param Xtractor_Http_Body $body
+   */
   public function setRequestBody(Xtractor_Http_Body $body)
   {
     $this->requestBody = $body;
   }
 
+  /**
+   * @return mixed
+   */
   public function getRequestBody()
   {
     return $this->requestBody;
   }
 
+  /**
+   * @param Xtractor_Http_Options $options
+   */
   public function setRequestOptions(Xtractor_Http_Options $options)
   {
     $this->requestOptions = $options;
   }
 
+  /**
+   * @return mixed
+   */
   public function getRequestOptions()
   {
     return $this->requestOptions;
--- src/Xtractor/Http/Response.php
+++ src/Xtractor/Http/Response.php
@@ -4,14 +4,42 @@ if (!class_exists('Xtractor_Client')) {
   require_once dirname(__FILE__) . '/../autoload.php';
 }
 
+/**
+ * Class Xtractor_Http_Response
+ *
+ * This class is designed like a business object but includes some methods
+ * to parse responses from api.
+ */
 class Xtractor_Http_Response
 {
+  /**
+   * @var null|string
+   */
   private $_responseCode = null;
+  /**
+   * @var null|integer
+   */
   private $_headerSize = null;
+  /**
+   * @var null|float
+   */
   private $_totalTime = null;
+  /**
+   * @var null|string
+   */
   private $_rawData = null;
+  /**
+   * @var array
+   */
   private $_parsedResult = array();
 
+  /**
+   * @param $responseCode
+   * @param $headerSize
+   * @param $totalTime
+   * @param $responseData
+   *
+   */
   public function __construct($responseCode, $headerSize, $totalTime, $responseData)
   {
     $this->setResponseCode($responseCode);
@@ -21,82 +49,129 @@ class Xtractor_Http_Response
     $this->parse();
   }
 
+  /**
+   * @return null
+   */
   public function getResponseCode()
   {
     return $this->_responseCode;
   }
 
+  /**
+   * @return mixed
+   */
   public function getResponseHeader()
   {
     return $this->_parsedResult['responseHeader'];
   }
 
+  /**
+   * @param $responseHeader
+   */
   public function setResponseHeader($responseHeader)
   {
     $this->_parsedResult['responseHeader'] = $responseHeader;
   }
 
+  /**
+   * @return mixed
+   */
   public function getResponseBody()
   {
     return $this->_parsedResult['responseBody'];
   }
 
+  /**
+   * @param $responseBody
+   */
   public function setResponseBody($responseBody)
   {
     $this->_parsedResult['responseBody'] = $responseBody;
   }
 
+  /**
+   * @param $responseCode
+   */
   private function setResponseCode($responseCode)
   {
     $this->_responseCode = $responseCode;
   }
 
+  /**
+   * @param $responseData
+   */
   private function setResponseRawData($responseData)
   {
     $this->_rawData = $responseData;
   }
 
+  /**
+   * @param $headerSize
+   */
   private function setHeaderSize($headerSize)
   {
     $this->_headerSize = $headerSize;
   }
 
+  /**
+   * @param $totalTime
+   */
   private function setTotalTime($totalTime)
   {
     $this->_totalTime = $totalTime;
   }
 
+  /**
+   * @return string
+   */
   public function getTotalTime()
   {
     return (String) $this->_totalTime;
   }
 
+  /**
+   * This method parses all raw data and set them to class properties.
+   */
   private function parse()
   {
+    //If we know the header size we can make a clean split in response data.
     if ($this->_headerSize) {
       $responseBody = substr($this->_rawData, $this->_headerSize);
       $responseHeader = substr($this->_rawData, 0, $this->_headerSize);
     } else {
+      //Try to guess the right position for data split.
       $responseSegments = explode("\r\n\r\n", $this->_rawData, 2);
       $responseHeader = $responseSegments[0];
       $responseBody = isset($responseSegments[1]) ? $responseSegments[1] : null;
     }
 
-    $responseHeader = $this->getHttpResponseHeader($responseHeader);
+    //Parse response header based on data type
+    $parsedResponseHeader = $this->parseHttpResponseHeader($responseHeader);
 
-    $this->setResponseHeader($responseHeader);
+    $this->setResponseHeader($parsedResponseHeader);
     $this->setResponseBody($responseBody);
   }
 
-  private function getHttpResponseHeader($rawHeaders)
+  /**
+   * @param $responseHeader
+   * @return array
+   *
+   * Calculates the data typ of given $responseHeader and
+   * lead data to the matching parse method.
+   */
+  private function parseHttpResponseHeader($responseHeader)
   {
-    if (is_array($rawHeaders)) {
-      return $this->parseArrayHeaders($rawHeaders);
+    if (is_array($responseHeader)) {
+      return $this->parseArrayHeaders($responseHeader);
     } else {
-      return $this->parseStringHeaders($rawHeaders);
+      return $this->parseStringHeaders($responseHeader);
     }
   }
 
+  /**
+   * @param $rawHeaders
+   * @return array
+   */
   private function parseStringHeaders($rawHeaders)
   {
     $headers = array();
@@ -115,6 +190,10 @@ class Xtractor_Http_Response
     return $headers;
   }
 
+  /**
+   * @param $rawHeaders
+   * @return array
+   */
   private function parseArrayHeaders($rawHeaders)
   {
     $header_count = count($rawHeaders);
@@ -122,6 +201,7 @@ class Xtractor_Http_Response
 
     for ($i = 0; $i < $header_count; $i++) {
       $header = $rawHeaders[$i];
+
       // Times will have colons in - so we just want the first match.
       $header_parts = explode(': ', $header, 2);
       if (count($header_parts) == 2) {
