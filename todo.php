c4f0216fe4b9a677c09aee02d63fed3af32ed5a4
Thilo Habenreich <t.habenreich@web.de>
2015-07-09 18:34:57 +0200

    Add a todo file to see what's next.

---
+++ todo.php
@@ -0,0 +1,14 @@
+<?php
+//@TODO: Make them PSR-4 compatible
+//@TODO: Add unit tests (PHPUnit)
+//@TODO: Check changes for API v2
+//@TODO: Add documentation
+//@TODO: Use namespaces
+//@TODO: Add project to composer
+//@TODO: Make developer documentation to override provided or add API methods
+//@TODO: Try to make exception handling more easy
+//@TODO: Check code against PSR-1 & PSR-2
+//@TODO: Provide a PSR-3 compatible logger interface
+//@TODO: Add more advanced examples
+//@TODO: Add a simple script/class/method to signup our API
+//@TODO: Add more stable/improved access token validation
