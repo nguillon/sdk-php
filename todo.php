37be877ce03d2e7fb609e78382fcfe8e32d50338
Thilo Habenreich <t.habenreich@web.de>
2015-07-16 16:32:52 +0200

    Remove unused file

--- todo.php
+++
@@ -1,9 +0,0 @@
-<?php
-//@TODO: Make user documentation to override provided or add API methods
-//@TODO: Add more advanced examples
-//Example Project
-//@TODO: Provide upload_directory
-
-//@TODO: Repository versioning
-//@TODO: Add project to composer
-
