4df4cb8a6bee30afa6d84ba4d86748711edabe9f
Christian Neuhäuser <chris@type10.com>
2015-11-05 10:05:30 +0100

    AR-4417: Updated examples

---
+++ examples/Gateway_getGatewayItemList.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../xtractor.php';
+
+$xtractorApi = new Xtractor('<YOUR_API_KEY>');
+$file = __DIR__ . '/files/example.pdf';
+
+try {
+  $gatewayApi = new \Swagger\Client\Api\GatewayApi();
+  $payload = new \Swagger\Client\Model\GatewayItemListPayload([
+      'type' => '<GATEWAY_TYPE>',
+      'auth' => [
+          'username' => '<YOUR_GATEWAY_USERNAME>',
+          'password' => '<YOUR_GATEWAY_PASSWORD>'
+      ]
+  ]);
+  $result = $gatewayApi->getGatewayItemList($payload);
+
+  echo "<pre>";
+  var_export($result);
+  echo "</pre>";
+}
+catch (Swagger\Client\ApiException $e) {
+  echo "<pre>\n";
+  echo 'Caught exception: ', $e->getMessage(), "\n";
+  echo 'HTTP status code: ', $e->getCode(), "\n";
+  echo 'HTTP response headers: ', $e->getResponseHeaders(), "\n";
+  echo 'HTTP response body: '; print_r($e->getResponseBody()); echo "\n";
+  echo "</pre>\n";
+}
--- examples/Semantics_getDocumentSemantics.php
+++ examples/Semantics_getDocumentSemantics.php
@@ -8,17 +8,17 @@ $file = __DIR__ . '/files/example.pdf';
 try {
   $client = $xtractorApi->getApiClient();
 
-  $result = $client->documentSemantics($file);
+  $result = $client->getDocumentSemantics($file);
 
   echo "<pre>";
   var_export($result);
   echo "</pre>";
 }
 catch (Swagger\Client\ApiException $e) {
-  echo "<pre>";
+  echo "<pre>\n";
   echo 'Caught exception: ', $e->getMessage(), "\n";
-  echo 'HTTP response headers: ', $e->getResponseHeaders(), "\n";
-  echo 'HTTP response body: ', $e->getResponseBody(), "\n";
   echo 'HTTP status code: ', $e->getCode(), "\n";
-  echo "</pre>";
+  echo 'HTTP response headers: ', $e->getResponseHeaders(), "\n";
+  echo 'HTTP response body: '; print_r($e->getResponseBody()); echo "\n";
+  echo "</pre>\n";
 }
